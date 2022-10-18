4c82034289eb53e721de0134250828bfa6ba6860
smha-Promedius <smha@promedius.ai>
2022-10-18 20:25:33 +0900

    version 0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'classification_models_pytorch',
   packages = find_packages(exclude=['examples']),
-  version = '0.0.2',
+  version = '0.0.3',
   license='MIT',
   description = 'classification_models_pytorch',
   long_description_content_type = 'text/markdown',
